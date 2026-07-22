69135356f5de4a0abe9a8bc5aba8d8b609598290
Heitor-Souza-Pacheco <heitorpacheco2009@gmail.com>
2026-07-22 09:49:44 -0300

    fix - buildscript

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -24,7 +24,12 @@ tasks.register<Delete>("clean") {
 }
 
 buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
     dependencies {
         classpath("com.google.gms:google-services:4.4.2")
     }
 }
+
